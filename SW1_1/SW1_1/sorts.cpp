47fd3b9697f00b524400d360d04013260fbe9c54
zxcdotagul <aleksandrkrupennikov33@gmail.com>
2026-03-22 20:32:04 +0300

    сделал первое задание

---
+++ SW1_1/SW1_1/sorts.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "sorts.h"
+
+int getMax(const int* ar, int size) {
+    int ind_max = 0;
+    for (int i = 1; i < size; i++) {
+        if (ar[ind_max] < ar[i]) {
+            ind_max = i;
+        }
+    }
+    return ind_max; 
+}
+
+void selectionSort(int* ar, int size) {
+    for (int i = 0; i < size; i++) {
+        int indMax = getMax(ar, size - i);
+        std::swap(ar[size - i - 1], ar[indMax]);
+    } 
+}
+
+void selSt(int* r, int s, bool a) {
+    for (int i = 0; i < s; i++) {
+        int indMax = getMax(&r[a == true ? 0 : i], s - i);
+        std::swap(r[a == true ? indMax : indMax + i], r[a == true ? s - i - 1 : i]);
+    }
+}
---
+++ SW1_1/SW1_1/sorts.h
@@ -0,0 +1,13 @@
+#ifndef _sorts_h_
+#define _sorts_h_
+
+#include <iostream>
+
+
+int getMax(const int*, int);
+void selectionSort(int* ar, int size);
+
+
+void selSt(int* r, int s, bool a);
+
+#endif
