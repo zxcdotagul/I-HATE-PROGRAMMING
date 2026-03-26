6d2c6c7e246a99df4c92528e338bf2e37886f954
zxcdotagul <aleksandrkrupennikov33@gmail.com>
2026-03-26 22:34:00 +0300

    ТРЕТЬЕ ЗАДАНИЕ

---
+++ SW_3/SW_3/file_operations.cpp
@@ -0,0 +1,32 @@
+#include "file_operations.h"
+
+void fillFile(const char* fileName, int min, int max, char delimeter, int cols, int countElements) {
+    std::ofstream file(fileName);
+
+    if (!file.is_open()) 
+       return;
+    
+
+    srand(time(NULL));
+
+    int* ar = new int[countElements];
+
+    for (int i = 0; i < countElements; i++) {
+        ar[i] = min + rand() % (max - min + 1);
+    }
+
+    for (int i = 0; i < countElements; i++) {
+        file << ar[i];
+
+        if ((i + 1) % cols == 0) {
+            file << std::endl;
+        }
+        else if (i != countElements - 1) {
+            file << delimeter;
+        }
+    }
+
+    delete[] ar;
+    file.close();
+   
+}
---
+++ SW_3/SW_3/file_operations.h
@@ -0,0 +1,16 @@
+#ifndef _file_operations_
+#define _file_operations_
+
+//here write needed includes
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+void fillFile(const char* fileName, int min, int max, char delimeter, int cols, int countElements);	//< task4 SW_3
+
+
+
+
+
+#endif
