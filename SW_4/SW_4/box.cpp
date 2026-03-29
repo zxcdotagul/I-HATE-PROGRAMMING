9c0b2e3d305222a3f242e5c5192ed31b83630d5f
zxcdotagul <aleksandrkrupennikov33@gmail.com>
2026-03-29 12:26:15 +0300

    СДЕЛАЛ ПЕРВЛОЕ ЗАДАНИЕ

---
+++ SW_4/SW_4/box.cpp
@@ -0,0 +1,35 @@
+#include "box.h"
+#include <fstream>
+#include <iostream>
+
+void writeToFile(const char* fileName, const struct Box& box) {
+    std::ofstream file(fileName, std::ios::binary);
+    if (!file.is_open()) {
+        return;
+    }
+
+    file.write(reinterpret_cast<const char*>(&box._len), sizeof(box._len));
+    file.write(reinterpret_cast<const char*>(&box._width), sizeof(box._width));
+    file.write(reinterpret_cast<const char*>(&box._height), sizeof(box._height));
+    file.write(reinterpret_cast<const char*>(&box._color), sizeof(box._color));
+
+    file.close();
+}
+
+void readFromFile(const char* fileName, struct Box& box) {
+    if (&box == nullptr) {
+        return;
+    }
+
+    std::ifstream file(fileName, std::ios::binary);
+    if (!file.is_open()) {
+        return;
+    }
+
+    file.read(reinterpret_cast<char*>(&box._len), sizeof(box._len));
+    file.read(reinterpret_cast<char*>(&box._width), sizeof(box._width));
+    file.read(reinterpret_cast<char*>(&box._height), sizeof(box._height));
+    file.read(reinterpret_cast<char*>(&box._color), sizeof(box._color));
+
+    file.close();
+}
