a8807c29eced5ded8183337df20c44bcb4228194
zxcdotagul <aleksandrkrupennikov33@gmail.com>
2026-04-14 20:45:56 +0300

    ejwfuwefuewf

---
+++ sorts.h
@@ -0,0 +1,11 @@
+#ifndef sorts_h
+#define sorts_h
+
+#include <iostream>
+
+int getMax(const int*, int);
+void selectionSort(int* ar, int size);
+void selSt(int* ar, int size, bool ascending); 
+
+
+#endif
---
+++ test_sorts.h
@@ -0,0 +1,10 @@
+#ifndef _test_sorts_h_
+#define _test_sorts_h_
+
+#include <iostream>
+
+void test_selectionSort();
+int isSorted(const int* array, unsigned int size); 
+void test_isSorted();
+
+#endif
