bf5ded40e0ab6f39be472db8a78ab2b2eadbd889
zxcdotagul <aleksandrkrupennikov33@gmail.com>
2026-04-20 17:19:58 +0300

    Add files via upload
    типа йоу

---
+++ box.h
@@ -0,0 +1,14 @@
+ #ifndef BOX_H
+#define BOX_H
+
+struct Box {
+    int _len;    // длина
+    int _width;  // ширина
+    int _height; // высота
+    int _color;  // цвет
+};
+
+void writeToFile(const char* fileName, const struct Box& box);
+void readFromFile(const char* fileName, struct Box& box);
+
+#endif // BOX_H
