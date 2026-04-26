87b555a6ae263a554b930e875580145aeefd7b15
zxcdotagul <aleksandrkrupennikov33@gmail.com>
2026-04-26 17:09:12 +0300

    св 2 св 3 св5 2 задание

--- bit_operations.cpp
+++ bit_operations.cpp
@@ -6,7 +6,7 @@ void swapHalfWord(unsigned int* words) {
 }
 void cycleShift(int* number, int N, int left) {
     N = N % 32;
-    if (N == 0) return; 
+    if (N == 0) return;
     unsigned int num = *number;
 
     if (left) {
@@ -39,7 +39,7 @@ int getMaxBit(const int* array, int N) {
         }
     }
 
-    return maxIndex; 
+    return maxIndex;
 }
 unsigned char getCount1Bit(int number) {
     unsigned int num = number;
@@ -51,4 +51,61 @@ unsigned char getCount1Bit(int number) {
     }
 
     return count;
+}
+
+void cycleShiftArray(int* array, int size, int N, bool left) {
+    if (size == 0 || N == 0) return;
+
+    N = N % 32;
+
+    if (left) {
+        
+        unsigned int carry = 0;
+        for (int i = 0; i < size; i++) {
+            unsigned int val = (unsigned int)array[i];
+            unsigned int next = (val << N) | carry;
+            carry = val >> (32 - N);
+            array[i] = (int)next;
+        }
+       
+        if (carry) {
+            array[0] = (int)((unsigned int)array[0] | carry);
+        }
+    }
+    else {
+       
+        unsigned int carry = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            unsigned int val = (unsigned int)array[i];
+            unsigned int next = (val >> N) | carry;
+            carry = val << (32 - N);
+            array[i] = (int)next;
+        }
+       
+        if (carry) {
+            array[size - 1] = (int)((unsigned int)array[size - 1] | carry);
+        }
+    }
+}
+
+void setBit(int* number, int numBit) {
+    *number |= (1 << numBit);
+}
+
+void clearBit(int* number, int numBit) {
+    *number &= ~(1 << numBit);
+}
+bool isPalindromNumber(const int number) {
+    unsigned int num = static_cast<unsigned int>(number);
+
+    for (int i = 0; i < 16; i++) {
+        int leftBit = (num >> (31 - i)) & 1;
+        int rightBit = (num >> i) & 1;
+
+        if (leftBit != rightBit) {
+            return false;
+        }
+    }
+
+    return true;
 }
---
+++ file_operations.cpp
@@ -0,0 +1,43 @@
+#include "file_operations.h"
+#include <fstream>;
+#include <iostream>
+#include <cstdlib>  
+#include <ctime> 
+void fillFile(const char* fileName, int min, int max, char delimeter, int cols, int countElements) {
+    std::ofstream file(fileName);
+
+    if (!file.is_open())
+        return;
+
+
+    srand(time(NULL));
+
+    int* ar = new int[countElements];
+
+    for (int i = 0; i < countElements; i++) {
+        ar[i] = min + rand() % (max - min + 1);
+    }
+
+    for (int i = 0; i < countElements; i++) {
+        file << ar[i];
+
+        if ((i + 1) % cols == 0) {
+            file << std::endl;
+        }
+        else if (i != countElements - 1) {
+            file << delimeter;
+        }
+    }
+
+    delete[] ar;
+    file.close();
+
+    std::ifstream readFile(fileName);
+
+
+    char ch;
+    while (readFile.get(ch)) {
+        std::cout << ch;
+    }
+
+}
